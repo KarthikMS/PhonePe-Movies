f1168e3c70e6e2100ee55094f23977001f52ba46
Karthik <mskarthik6@gmail.com>
2023-09-24 13:21:12 +0100

    List of movie titles shown.

--- PhonePe Movies/PopularMoviesScreen/PopularMoviesViewController.swift
+++ PhonePe Movies/PopularMoviesScreen/PopularMoviesViewController.swift
@@ -30,6 +30,65 @@ final class PopularMoviesViewController: UITableViewController {
         super.viewDidLoad()
 
         title = "Popular Movies"
+        configureTableView()
+        bindViewModel()
+        viewModel.fetchData()
+    }
+    
+    // MARK: - Setup
+    
+    private func configureTableView() {
+        tableView.registerClassWithDefaultIdentifier(UITableViewCell.self)
+        tableView.rowHeight = UITableView.automaticDimension
+    }
+    
+    private func bindViewModel() {
+        viewModel.didUpdateState = { [weak self] in
+            DispatchQueue.main.async {
+                self?.renderCurrentState()
+            }
+        }
+    }
+    
+    // MARK: - State rendering
+    
+    private func renderCurrentState() {
+        switch viewModel.state {
+        case .loading:
+            // TODO: Render Loading state.
+            break
+        case .loaded:
+            tableView.reloadData()
+        case .error:
+            showError()
+        }
+    }
+    
+    private func showError() {
+        let alertController = UIAlertController(title: "Could not fetch movies", message: nil, preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "Retry", style: .default) { [weak self] _ in
+            self?.viewModel.fetchData()
+        }
+        alertController.addAction(alertAction)
+        present(alertController, animated: true, completion: nil)
+    }
+
+}
+
+// MARK: - UITableViewDataSource
+extension PopularMoviesViewController {
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        viewModel.state.movies.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCellWithDefaultIdentifier() as? UITableViewCell else {
+            return UITableViewCell()
+        }
+        let movie = viewModel.state.movies[indexPath.row]
+        cell.textLabel?.text = movie.title
+        return cell
     }
 
 }
--- PhonePe Movies/PopularMoviesScreen/PopularMoviesViewModel.swift
+++ PhonePe Movies/PopularMoviesScreen/PopularMoviesViewModel.swift
@@ -5,11 +5,41 @@
 //  Created by Karthik Maharajan Skandarajah on 24/09/2023.
 //
 
+enum PopularMoviesViewState {
+    case loading
+    case loaded([Movie])
+    case error
+}
+
+extension PopularMoviesViewState {
+    
+    var movies: [Movie] {
+        switch self {
+        case .loaded(let movies):
+            return movies
+        case .loading, .error:
+            return []
+        }
+    }
+    
+}
+
+
 final class PopularMoviesViewModel {
     
     // MARK: - Dependencies
     
     private let apiManager: APIManaging
+    
+    // MARK: - Properties
+    
+    var didUpdateState: (() -> Void)?
+
+    var state: PopularMoviesViewState = .loading {
+        didSet {
+            didUpdateState?()
+        }
+    }
     
     // MARK: - Init
     
@@ -17,4 +47,17 @@ final class PopularMoviesViewModel {
         self.apiManager = apiManager
     }
     
+    // MARK: - Network
+    
+    func fetchData() {
+        apiManager.execute(.fetchPopularMovies) { [weak self] result in
+            switch result {
+            case .success(let page):
+                self?.state = .loaded(page.results)
+            case .failure:
+                self?.state = .error
+            }
+        }
+    }
+
 }
